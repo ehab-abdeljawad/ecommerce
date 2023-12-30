02e9271f3d78829ffa2220a56fa8e950a674d079
ehab-abdeljawad <155148852+ehab-abdeljawad@users.noreply.github.com>
2023-12-30 11:56:44 +0200

    Add files via upload

---
+++ project_js/javascript/contact.js
@@ -0,0 +1,91 @@
+
+var user =document.getElementById("user")
+function check_user(){
+    var pattern = /^[a-zA-Z]*$/
+    var str = user.value
+
+    if(pattern.test(str)){
+        var e_span =document.getElementById("suser")
+        e_span.textContent=" "
+    }else{
+        var e_span =document.getElementById("suser")
+        e_span.textContent="Invalid username. Username should only contain letters."
+        e_span.style.color="red"
+    }
+    
+
+
+}
+user.addEventListener("input",check_user)
+
+var email =document.getElementById("email")
+function check_email(){
+    var str = email.value
+ 
+    var test = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    var res = test.test(str)
+    if(res){
+        var e_span =document.getElementById("semail")
+        e_span.textContent=" "
+        
+    }else{
+        var e_span =document.getElementById("semail")
+        e_span.textContent=" enter invaild email" 
+        e_span.style.color="red"
+    }
+
+
+}
+email.addEventListener("input",check_email)
+
+var text =document.getElementById("text")
+function check_text(){
+   var pattern = /^[^#%&$]*$/
+
+   if(pattern.test(text.value)){
+    var e_span =document.getElementById("stext")
+        e_span.textContent=" "
+   }else{
+    var e_span =document.getElementById("stext")
+    e_span.textContent=" don't enter spacial character !" 
+    e_span.style.color="red"
+   }
+}
+
+text.addEventListener("input",check_text)
+
+var button = document.getElementById("btn")
+
+function check(){
+    var u =user.value
+    var e = email.value
+    var t = text.value 
+ 
+    var test = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    var res = test.test(e)
+    var pattern = /^[a-zA-Z]*$/
+    var str = user.value
+    var user_ch =pattern.test(str)
+    if(!user_ch||!res||t === ''){
+        const happyEmoji = '\u{1F600}';
+        var massage="full form  please" + '\u{1F614}'
+        alert(massage)
+       
+    }else{
+        const happyEmoji = '\u{1F600}';
+        var massage="Thank You " + happyEmoji+'\u{2764}'
+        alert(massage)
+        location.reload()
+    }
+}
+
+button.addEventListener("click",check)
+
+
+var up =document.getElementById("foot")
+up.addEventListener("click", function () {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+})
+
+
+
